2ae49a92545befef9396a42b40d7ec18f89a12bf
ppyaya <kelvnyaya8@gmail.com>
2023-07-26 20:40:29 +0200

    aster working addTipper

--- src/components/addSideTipper.js
+++ src/components/addSideTipper.js
@@ -1,16 +1,85 @@
 import React from "react"
+import { db } from "./config/fireBase"
+import { collection , addDoc } from "firebase/firestore"
 
 function SideTipper(){
+
+const [formData , setFormData] = React.useState({
+  CompanyName : "",
+  onLoading : 6,
+  onDelivery : 9,
+  fromLocation : "",
+  toLocation : ""
+})
+
+  function handleChange(event){
+    const {name , value} = event.target
+    setFormData(prevFormDaata =>{
+      return{
+        ...prevFormDaata,
+        [name] : value
+      }
+    })
+  } 
+    // connect to data base
+    const SideTipperDB = collection(db , "sideTippers")
+
+  const handleSubmit = async(event)=>{
+    event.preventDefault()
+    try{
+
+      await addDoc(SideTipperDB , {
+
+        onDelivery :formData.onDelivery,
+        CompanyName : formData.CompanyName,
+        fromLocation : formData.fromLocation,
+        onLoading : formData.onLoading,
+        toLocation : formData.toLocation
+
+      } )
+
+    }catch(err){
+      console.log(err)
+    }
+  }
+
   return(
     
-    <div className="dropDown">
+    <form className="dropDown" onSubmit={handleSubmit}>
 
       <input
-        placeholder="side Tipper"              
+        placeholder="Side Tipper"
+        onChange={handleChange}
+        name="CompanyName"    
+        value={formData.CompanyName}          
       />
-      <input/>
 
-    </div>
+        <input
+        placeholder="loading ammount"  
+        onChange={handleChange}              
+        name="onLoading"
+        value={formData.onLoading}
+      />
+      <input
+        placeholder="delivery ammount"              
+        onChange={handleChange}  
+        name="onDelivery"   
+        value={formData.onDelivery}         
+      />
+      <input
+        placeholder="from location"              
+        onChange={handleChange}   
+        name="fromLocation" 
+        value={formData.fromLocation}          
+      />
+      <input
+        placeholder="to location"              
+        onChange={handleChange}
+        name="toLocation"   
+        value={formData.toLocation}           
+      />
+      <button>submit</button>
+    </form>
   )
 }
 
